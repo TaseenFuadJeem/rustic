d490cfb85abaa8b340cba78975b9846969d067a6
Heezol06 <shahriarrahmanheezol@gmail.com>
2022-06-05 19:12:44 +0600

    fc from heezol

--- src/Components/Home/RoadMap.js
+++ src/Components/Home/RoadMap.js
@@ -5,6 +5,8 @@ const RoadMap = () => {
         <div>
             <div>
                 <h1 className='text-center '>Road Map</h1>
+                <h2>The Journey of
+                    Monteno NFT</h2>
             </div>
         </div>
     );
