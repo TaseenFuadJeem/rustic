80d01de3d528ac06c8996165cf97820635d0690d
Heezol06 <shahriarrahmanheezol@gmail.com>
2022-06-05 19:10:35 +0600

    fc from heezol

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Home from './Components/Home/Home';
 import NftPortfolio from './Components/Home/NftPortfolio';
+import RoadMap from './Components/Home/RoadMap';
 
 function App() {
   return (
     <div>
       <Home />
       <NftPortfolio />
+      <RoadMap></RoadMap>
     </div>
   );
 }
---
+++ src/Components/Home/RoadMap.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const RoadMap = () => {
+    return (
+        <div>
+            <div>
+                <h1 className='text-center '>Road Map</h1>
+            </div>
+        </div>
+    );
+};
+
+export default RoadMap;
