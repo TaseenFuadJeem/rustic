16f37dfe691ac89eeb8662d5224b4f354ff74038
Md. Ta-Seen Fuad Jeem <mdtaseenfuad@gmail.com>
2022-06-05 18:02:17 +0600

    Created sample nft folder and imported the nft pics

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import Home from './Components/Home/Home';
-import NftPortfolio from './Components/Home/NftPortfolio';
 
 function App() {
   return (
     <div>
       <Home />
-      <NftPortfolio />
     </div>
   );
 }
---
+++ src/Components/Home/NftForHomePage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import nft1 from '../../Assets/nft (1).jpg';
+import nft2 from '../../Assets/nft (2).jpg';
+import nft3 from '../../Assets/nft (3).jpg';
+import nft4 from '../../Assets/nft (4).jpg';
+import nft5 from '../../Assets/nft (5).jpg';
+import nft6 from '../../Assets/nft (6).jpg';
+import nft7 from '../../Assets/nft (7).jpg';
+import nft8 from '../../Assets/nft (8).jpg';
+
+const NftForHomePage = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default NftForHomePage;
